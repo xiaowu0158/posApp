ba461b4cc691575baa868ea6c5bb95cf4ed3e70a
xiaowu0158 <87408696@qq.com>
2017-12-02 17:23:52 +0800

    修改测试

--- MyApplication/ocr_ui/src/main/java/com/baidu/ocr/ui/util/DimensionUtil.java
+++ MyApplication/ocr_ui/src/main/java/com/baidu/ocr/ui/util/DimensionUtil.java
@@ -8,6 +8,7 @@ import android.content.res.Resources;
 public class DimensionUtil {
 
     public static int dpToPx(int dp) {
+        System.out.println("测试");
         return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
     }
 
